fcb11ecea82de31ad03d22a5db32f2a4af201dc8
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-19 16:37:42 +0100

    started the main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// need to create a variable to get the element from the HTML
+const numberOne = document.getElementById("one");
+const numberButtons = document.querySelectorAll(".caclulator__button--number");
+const numberDisplay = document.querySelector(".calculator__display");
+
+console.log(numberButtons);
+
+// create a function that will create the event
+// innerHTML
+// innerText
+// +=
+const showNumber = (event) => {
+numberDisplay.innerHTML += event.target.innerText;
+}
+
+// conduct the action on the web where when clicking the button, it will display the number
+numberOne.addEventListener("click",showNumber);
+
+
+// it will go through each numbers and display it website
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener("click",showNumber);
+}
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click",showNumber);
+})
+
