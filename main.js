f3d6b5eb600390d3f5132f30efa3d61f2b51e957
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-22 19:37:59 +0100

    add the action for the click button

--- main.js
+++ main.js
@@ -4,14 +4,14 @@
 
 // need to create a variable to get the element from the HTML
 const numberOne = document.getElementById("one");
-const deleteButton = document.querySelector(".calculator__button--delete")
+const deleteButton = document.querySelector(".calculator__button--delete");
 const numberButtons = document.querySelectorAll(".calculator__button--number");
 const numberDisplay = document.querySelector(".calculator__display");
 const clearButtons = document.querySelector(".calculator__button--clear");
 const mathOperator = document.querySelector(".calculator__button--addition");
 const equalButton = document.querySelector(".calculator__button--equals");
 const prevNumber = document.querySelectorAll(".calculator__button--number");
-const currNumber= document.querySelectorAll(".calculator__button--number");
+const currNumber = document.querySelectorAll(".calculator__button--number");
 
 // console.log(numberButtons);
 
@@ -25,8 +25,16 @@ const showNumber = (event) => {
 
 console.log(showNumber);
 
+const clearNumber = (event) => {
+  clearButtons.innerHTML = "";
+};
+
 // conduct the action on the web where when clicking the button, it will display the number
 numberOne.addEventListener("click", showNumber);
+
+// conduct the action on the web to clear the display number when clicked the clear button
+
+clearButtons.addEventListener("click", clearNumber);
 
 // it will go through each numbers and display the number on the website when clicked on.
 for (let i = 0; i < numberButtons.length; i++) {
