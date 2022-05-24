efda27f345eeacca94e16079ed1f4064fe30d50a
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-24 15:14:58 +0100

    made changes in commit

--- main.js
+++ main.js
@@ -3,14 +3,11 @@
 // research math collocation javascript as a function
 
 // need to create a variable to get the element from the HTML
-const numberOne = document.getElementById("one");
 const deleteButton = document.querySelector(".calculator__button--delete");
 const numberButtons = document.querySelectorAll(".calculator__button--number");
 const numberDisplay = document.querySelector(".calculator__display");
 const clearButtons = document.querySelector(".calculator__button--clear");
 const equalButton = document.querySelector(".calculator__button--equals");
-const prevNumber = document.querySelectorAll(".calculator__button--number");
-const currNumber = document.querySelectorAll(".calculator__button--number");
 const mathOperator = document.querySelectorAll(".calculator__button--operator");
 
 console.log(numberButtons);
@@ -30,7 +27,7 @@ const showNumber = (event) => {
   tempArray.push(event.target.value);
   firstNumber = tempArray[0];
   secondNumber = tempArray[2];
-  numberDisplay.innerHTML += tempArray.join("");
+  numberDisplay.innerHTML = tempArray.join("");
   console.log(firstNumber);
   console.log(secondNumber);
 };
@@ -45,15 +42,15 @@ const clearNumber = (event) => {
   numberDisplay.innerHTML = "";
 };
 // create a function that will delete the last digit number.
-const deleteNumber = (event) => {
-  numberDisplay.innerHTML = numberButtons.innerText - 1;
-};
+// const deleteNumber = (event) => {
+//   numberDisplay.innerHTML = numberButtons.innerText - 1;
+// };
 
 // it will show the operator key in the display
 // mathOperator.addEventListener("click", showOperator);
 
 // conduct the action on take away one number from the display
-deleteButton.addEventListener("click", deleteNumber);
+// deleteButton.addEventListener("click", deleteNumber);
 // conduct the action on the web where when clicking the button, it will display the number
 // numberOne.addEventListener("click", showNumber);
 
@@ -76,24 +73,24 @@ mathOperator.forEach((operator) => {
 
 // create a function that would do the math operators
 
-const total = () => {
-  // console.log("function ran");
-  // console.log(firstNumber);
-  // console.log(secondNumber);
-  switch (operator) {
-    case "+":
-      return firstNumber + secondNumber;
-      break;
-    case "-":
-      return firstNumber - secondNumber;
-      break;
-    case "÷":
-      return firstNumber / secondNumber;
-    case "x":
-      break;
-      return firstNumber * secondNumber;
-  }
-};
+// const total = () => {
+//   // console.log("function ran");
+//   // console.log(firstNumber);
+//   // console.log(secondNumber);
+//   switch (operator) {
+//     case "+":
+//       return firstNumber + secondNumber;
+//       break;
+//     case "-":
+//       return firstNumber - secondNumber;
+//       break;
+//     case "÷":
+//       return firstNumber / secondNumber;
+//     case "x":
+//       break;
+//       return firstNumber * secondNumber;
+//   }
+// };
 
 // equalButton.addEventListener("click", () => {
 //   total();
